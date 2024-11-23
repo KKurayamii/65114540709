3ac23654de6e3f9eca5b91a5be647fc04b28eec1
KKurayamii <anna.sa.65@ubu.ac.th>
2024-11-23 13:08:56 +0700

    Update partners.py

--- partners/partners.py
+++ partners/partners.py
@@ -4,18 +4,25 @@
 #    print('can open file')
 
 from openpyxl import load_workbook
-wb = load_workbook('Me & My partner.xlsx')
-print(wb.active)
-for row in wb.active:
-    values = [cell.value for cell in row]
-    if values[1] and values[1]:
-        pair = (values[2], values[1])
-    if values[2] < values[1]:
-        pair = (values[2], values[1])
-    if pair is not groups:
-        groups.append(pair)
 
-        print(pair)
-    print(values)
+if __name__ == '__main__':
+    wb = load_workbook('Me & My partner.xlsx')
+    sheet = wb.active
 
+    valid_pairs = []
+
+    for row in sheet:
+        values = [cell.value for cell in row]
+
+        if values[1] is not None and values[2] is not None:
+            pair = (values[1], values[2])
+
+            if values[1] < values[2]:
+                pair = (values[2], values[1])
+            if pair not in valid_pairs:
+                valid_pairs.append(pair)
+
+
+    print(f"Total number of valid pairs: {len(valid_pairs)}")
+    print(valid_pairs)
 
